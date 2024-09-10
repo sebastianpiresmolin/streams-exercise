dbab58d852385b57c0ca2bd74aa8fee9ce0e018e
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-09-10 17:06:42 +0200

    Starting over, decided to take another approach

--- src/main/java/org/entities/Product.java
+++ src/main/java/org/entities/Product.java
@@ -1,73 +1,14 @@
 package org.entities;
 
+import java.time.LocalDate;
 
 
-import java.util.Date;
-
-public class Product {
-    private static int id;
+public record Product (
+        int id;
     private String name;
     private Category category;
     private int rating;
-    private Date created;
-    private Date lastChange;
+    LocalDate createdDate;
+    LocalDate lastModifiedDate;
+) {}
 
-    public Product(int id, String name, Category category, int rating, Date created, Date lastChange) {
-        this.id = id;
-        this.name = name;
-        this.category = category;
-        this.rating = rating;
-        this.created = created;
-        this.lastChange = lastChange;
-    }
-
-    // Getters
-    public static int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public Date getLastChange() {
-        return lastChange;
-    }
-
-    // Setters
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-
-    public void setLastChange(Date lastChange) {
-        this.lastChange = lastChange;
-    }
-}
